902c27937e5be03b07bfb6c6816b78d0148a5889
shenyu <shenyu@shenyu.me>
2015-04-16 00:38:18 +0800

    Composer, Install kartik\date\DatePicker.

--- views/site/class.php
+++ views/site/class.php
@@ -1,5 +1,6 @@
 <?php
 use yii\helpers\Html;
+use kartik\date\DatePicker;
 
 /* @var $this yii\web\View */
 $this->title = '我的课表';
@@ -8,7 +9,27 @@ $this->params['breadcrumbs'][] = $this->title;
 <div class="site-about">
     <h1><?= Html::encode($this->title) ?></h1>
     
-    <?= Html::a('导出ics文件', NULL, ['class' => 'btn btn-primary', 'id' => "dump"]) ?>
+    <div class="row">
+        <div class="col-lg-3">
+        <?php
+        echo DatePicker::widget([
+            'name' => 'firstday',
+            'value' => '',
+            'pluginOptions' => [
+                'autoclose'=>true,
+                'format' => 'mm/dd/yyyy'
+            ],
+            'options' => [
+                'placeholder' => '开学第一天',
+            ],
+        ]);
+        ?>
+        </div>
+    
+        <div class="col-lg-5">
+        <?= Html::a('导出ics文件', NULL, ['class' => 'btn btn-primary', 'id' => "dump"]) ?>
+        </div>
+    </div>
     
     <div style="margin-top: 20px;">
     <?= $classese ?>
