c7644e6e75e018e443da61132811e1154c55db85
shenyu <shenyu@shenyu.me>
2015-04-18 00:46:34 +0800

    Site/About, Optimize format.

--- views/site/about.php
+++ views/site/about.php
@@ -8,34 +8,39 @@ $this->params['breadcrumbs'][] = $this->title;
 <div class="site-about">
     <h1><?= Html::encode($this->title) ?></h1>
     
-    
-    <p>
-        如果你对这个项目感兴趣，你可以 Fork 或 Clone 它的源码
-    </p>
-    
-    <p>
-        <span class="octicon octicon-mark-github"></span>
-        <!--<span class="mega-octicon octicon-mark-github"></span>-->
-        <a href="https://github.com/chrisniael/whatclass.cn" target="_blank">whatclass.cn</a>
-    </p>
+    <ul>
+        <li>
+            <p>
+                如果你对这个项目感兴趣，你可以 Fork 或 Clone 它的源码
+            </p>
 
-    <p>
-        如果需要帮助，可以在下面这些地方找到我
-    </p>
-    
-    <p>
-        <span class="octicon octicon-mark-github"></span>
-        <a href="https://github.com/chrisniael" target="_blank">chrisniael</a>
-    </p>
-    
-    <p>
-        <span class="octicon octicon-home"></span>
-        <a href="http://shenyu.me" target="_blank">http://shenyu.me</a>
-        
-    </p>
-    
-    <p>
-        <span class="octicon octicon-mail"></span>
-        <a href="mailto:shenyu@shenyu.me">shenyu@shenyu.me</a>
-    </p>
+            <p>
+                <span class="octicon octicon-mark-github"></span>
+                <!--<span class="mega-octicon octicon-mark-github"></span>-->
+                <a href="https://github.com/chrisniael/whatclass.cn" target="_blank">whatclass.cn</a>
+            </p>
+        </li>
+
+        <li>
+            <p>
+                如果需要帮助，可以在下面这些地方找到我
+            </p>
+
+            <p>
+                <span class="octicon octicon-mark-github"></span>
+                <a href="https://github.com/chrisniael" target="_blank">chrisniael</a>
+            </p>
+
+            <p>
+                <span class="octicon octicon-home"></span>
+                <a href="http://shenyu.me" target="_blank">http://shenyu.me</a>
+
+            </p>
+
+            <p>
+                <span class="octicon octicon-mail"></span>
+                <a href="mailto:shenyu@shenyu.me">shenyu@shenyu.me</a>
+            </p>
+        </li>
+    </ul>
 </div>
