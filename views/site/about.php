08e8ac38214a832a7d302021c72af5babff04e84
shenyu <shenyu@shenyu.me>
2015-04-17 22:30:17 +0800

    Site/About, complete.

--- views/site/about.php
+++ views/site/about.php
@@ -2,15 +2,40 @@
 use yii\helpers\Html;
 
 /* @var $this yii\web\View */
-$this->title = 'About';
+$this->title = '关于';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="site-about">
     <h1><?= Html::encode($this->title) ?></h1>
-
+    
+    
     <p>
-        This is the About page. You may modify the following file to customize its content:
+        如果你对这个项目感兴趣，你可以 Fork 或 Clone 它的源码
+    </p>
+    
+    <p>
+        <span class="octicon octicon-mark-github"></span>
+        <!--<span class="mega-octicon octicon-mark-github"></span>-->
+        <a href="https://github.com/chrisniael/whatclass.cn" target="_blank">whatclass.cn</a>
     </p>
 
-    <code><?= __FILE__ ?></code>
+    <p>
+        如果需要帮助，可以在下面这些地方找到我
+    </p>
+    
+    <p>
+        <span class="octicon octicon-mark-github"></span>
+        <a href="https://github.com/chrisniael" target="_blank">chrisniael</a>
+    </p>
+    
+    <p>
+        <span class="octicon octicon-home"></span>
+        <a href="http://shenyu.me" target="_blank">http://shenyu.me</a>
+        
+    </p>
+    
+    <p>
+        <span class="octicon octicon-mail"></span>
+        <a href="mailto:shenyu@shenyu.me">shenyu@shenyu.me</a>
+    </p>
 </div>
